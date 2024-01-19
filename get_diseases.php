034d2f4f6fb4e1b96e1f68d036840d48b3072096
Sudipto Bhattacharjee <ttsudipto@gmail.com>
2024-01-19 19:07:14 +0530

    Retrieve proteins for diseases

--- get_diseases.php
+++ get_diseases.php
@@ -1,6 +1,10 @@
 <?php
     include('db.php');
 
+    $concise = false;
+    if (array_key_exists("concise", $_GET))
+        if ($_GET["concise"] == "1")
+            $concise = true;
     $key = urldecode($_GET['key']);
 //     echo $key."<br/>";
 
@@ -19,6 +23,21 @@
     $diseaseStmt->execute();
     $diseaseRows = execute_and_fetch_assoc($diseaseStmt);
     $diseaseStmt->close();
+    if (! $concise) {
+        for($i=0; $i<count($diseaseRows); ++$i){
+            $proteinQuery = "select protein.UniProtAccession,GeneName from protein inner join protein_disease_association on protein.UniProtAccession=protein_disease_association.UniProtAccession where MADID=?;";
+//             echo $proteinQuery."<br/>";
+            $proteinStmt = $conn->prepare($proteinQuery);
+            $proteinStmt->bind_param("s", $diseaseRows[$i]["MADID"]);
+            $proteinStmt->execute();
+            $proteinRows = execute_and_fetch_assoc($proteinStmt);
+            $proteins = array();
+            for ($j=0; $j<count($proteinRows); ++$j)
+                array_push($proteins, $proteinRows[$j]["GeneName"]." (<a href=\"protein.php?keytype=ID&key=".$proteinRows[$j]["UniProtAccession"]."\">".$proteinRows[$j]["UniProtAccession"]."</a>)");
+            $diseaseRows[$i]["ProteinAssociations"] = implode("; ", $proteins);
+        }
+    }
+
     closeConnection($conn);
     $rowsJSON = json_encode($diseaseRows);
     echo $rowsJSON;
